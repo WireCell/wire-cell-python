8fa64dd3ea9f6dc1b3cab06fae0e4e6bf7f4efa2
Brett Viren <brett.viren@gmail.com>
2017-11-22 18:11:23 -0500

    Exhaustively plot chip conductors.  There's some bug.

--- wirecell/util/test/test_wireplots.py
+++ wirecell/util/test/test_wireplots.py
@@ -3,6 +3,7 @@ from wirecell.util.wires import apa, graph
 
 import networkx
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 desc = apa.Description();
 G,P = apa.graph(desc)
@@ -21,8 +22,13 @@ def test_plot_conductor():
     plt.savefig('test_plot_conductor.pdf')
 
 def test_plot_chip():
-    conductors = graph.neighbors_by_type(G, P.chip[0], 'conductor', 2)
-    assert conductors
-    wg, wpos = graph.conductors_graph(G, conductors)
-    networkx.draw(wg, pos=wpos, with_labels=True)
-    plt.savefig('test_plot_chip.pdf')
+    with PdfPages('test_plot_chip.pdf') as pdf:
+        for chip in P.chip:
+            print chip
+            plt.title(chip)
+            conductors = graph.neighbors_by_type(G, chip, 'conductor', 2)
+            assert conductors
+            wg, wpos = graph.conductors_graph(G, conductors)
+            networkx.draw(wg, pos=wpos)
+            pdf.savefig()
+            plt.close()
