153fc0a7aeb822195f221e3a4e04a3a04750601b
Brett Viren <brett.viren@gmail.com>
2017-11-22 20:49:15 -0500

    More and working tests.

--- wirecell/util/test/test_wireplots.py
+++ wirecell/util/test/test_wireplots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from wirecell.util.wires import apa, graph
-
+from itertools import chain
+from collections import defaultdict
 import networkx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -26,9 +27,61 @@ def test_plot_chip():
         for chip in P.chip:
             print chip
             plt.title(chip)
-            conductors = graph.neighbors_by_type(G, chip, 'conductor', 2)
-            assert conductors
+            channels = graph.neighbors_by_type(G, chip, 'channel')
+            conductors = chain.from_iterable([graph.neighbors_by_type(G, ch, 'conductor') for ch in channels])
             wg, wpos = graph.conductors_graph(G, conductors)
-            networkx.draw(wg, pos=wpos)
+            networkx.draw(wg, pos=wpos, arrows=False)
             pdf.savefig()
             plt.close()
+
+            
+
+def test_plot_board():
+    with PdfPages('test_plot_board.pdf') as pdf:
+        for board in P.board:
+            plt.title(board)
+
+            bg = networkx.DiGraph()
+            bpos = dict()
+            chips = graph.neighbors_by_type(G, board, 'chip')
+            print board, len(chips), chips
+            for chip in chips:
+                channels = graph.neighbors_by_type(G, chip, 'channel')
+                print chip, len(channels)
+                conductors = chain.from_iterable([graph.neighbors_by_type(G, ch, 'conductor') for ch in channels])
+                cg, cpos = graph.conductors_graph(G, conductors)
+                bg = networkx.compose(bg, cg)
+                bpos.update(cpos)
+
+            networkx.draw(bg, pos=bpos, arrows=False)
+            pdf.savefig()
+            plt.close()
+
+def test_plot_wib():
+    with PdfPages('test_plot_wib.pdf') as pdf:
+
+        boards_by_connector = defaultdict(list)
+        for wib in P.wib:
+            boards_on_wib = graph.neighbors_by_type(G, wib, 'board')
+            for b in boards_on_wib:
+                iconnector = G[wib][b]['connector']
+                boards_by_connector[iconnector].append((wib,b))
+        print boards_by_connector
+        for iconnector, wib_boards in sorted(boards_by_connector.items()):
+            for wib,board in wib_boards:
+                plt.title("%s connector %s %s" % (wib, iconnector, board))
+                bg = networkx.DiGraph()
+                bpos = dict()
+                chips = graph.neighbors_by_type(G, board, 'chip')
+                for chip in chips:
+                    channels = graph.neighbors_by_type(G, chip, 'channel')
+                    print chip, len(channels)
+                    conductors = chain.from_iterable([graph.neighbors_by_type(G, ch, 'conductor') for ch in channels])
+                    cg, cpos = graph.conductors_graph(G, conductors)
+                    bg = networkx.compose(bg, cg)
+                    bpos.update(cpos)
+                networkx.draw(bg, pos=bpos, arrows=False)
+                pdf.savefig()
+                plt.close()
+    
+            
