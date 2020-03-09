2811c3ad1cd80debfe4edcc9145c7f7de3ac7a46
Wenqiang Gu <goowenq@gmail.com>
2020-03-08 23:50:19 -0400

    fix some rounding bug for icarus and pdsp

--- wirecell/sigproc/response/plots.py
+++ wirecell/sigproc/response/plots.py
@@ -22,7 +22,7 @@ def get_plane(fr, planeid, reflect=True):
     pdelta = pitches[1] - pitches[0]
     pmax = max(map(abs, pitches)) + 0.5*pdelta   # bin centered
     pmin = -pmax
-    npbins = int(round((pmax-pmin)/pdelta))
+    npbins = int(round((pmax-pmin + 0.001)/pdelta))
     print 'PBINS:', npbins, pmin, pmax, pdelta, (pmax-pmin)/pdelta
 
     tlin = numpy.linspace(tmin, tmax, ntbins+1)
@@ -36,13 +36,10 @@ def get_plane(fr, planeid, reflect=True):
 
     for path in pr.paths:
         pitch = path.pitchpos
-        pind = int(round((pitch - pmin)/pdelta))
+        pind = int(round((pitch -0.001 - pmin)/pdelta) ) # minus 0.001 to avoid rounding issue
+        # print "pitch: ", pitch, " pmin: ", pmin, " pind: ", pind
         pind = max(0, pind)
-        pind = min(npbins, pind)
-
-        pind_ref = int(round((-pitch - pmin)/pdelta))
-        pind_ref = max(0, pind_ref)
-        pind_ref = min(npbins, pind_ref)
+        pind = min(npbins-1, pind)
 
         assert path.current.size == ntbins
         for tind, cur in enumerate(path.current):
@@ -61,14 +58,28 @@ def get_plane(fr, planeid, reflect=True):
             assert abs(perr) < 0.001*pdelta
             if pind >= npbins:
                 print 'pitch:', pind, pitch, pitches[pind, tind], perr
+            # print 'pitch:', pind, pitch, pitches[pind, tind], perr
 
             currents[pind, tind] = cur
 
             if reflect:
-                currents[pind_ref, tind] = cur
+                pind_ref = int(round((-pitch -0.001 - pmin)/pdelta))
+                if pind_ref>0 and pind_ref<npbins-1:
+                    currents[pind_ref, tind] = cur
 
     return (times, pitches, currents)
 
+def lg10(current):
+    # "log-10" style for response, see SP1 paper: https://arxiv.org/pdf/1802.08709.pdf
+    (tdim, pdim) = current.shape
+    c = numpy.zeros([tdim, pdim])
+    for tind in range(tdim):
+        for pind in range(pdim):
+            curr = -1.0*current[tind, pind]
+            if curr>1e-5: c[tind, pind] = numpy.log10(curr*1e5)
+            elif curr<-1e-5: c[tind, pind] = -numpy.log10(-curr*1e5)
+            else: c[tind, pind] = 0
+    return c
 
 def plot_planes(fr, filename=None):
     '''
@@ -82,31 +93,36 @@ def plot_planes(fr, filename=None):
 
     fig, axes = plt.subplots(3, 1, sharex=True, figsize=(8.0, 10.5))
 
-    fig.subplots_adjust(left=0.05, right=1.0, top=0.95, bottom=0.05)
+    fig.subplots_adjust(left=0.1, right=1.0, top=0.95, bottom=0.05)
 
-    vlims = [0.1, 0.1, 0.1]
+    # vlims = [0.1, 0.1, 0.1] # linear style
+    vlims = [4, 4, 4] # "log10" style
 
     for planeid in range(3):
         vlim = vlims[planeid]
         t, p, c = get_plane(fr, planeid)
         print t.shape, p.shape, c.shape
         ax = axes[planeid]
-        ax.axis([65, 90, -20, 20])
+        # ax.axis([65, 90, -20, 20])
+        pr = fr.planes[planeid]
+        pitches = [path.pitchpos for path in pr.paths]
+        pmax = max(map(abs, pitches))
+        ax.axis([0,70, -pmax,pmax])
         ax.set_title('Induced Current %s-plane' % 'UVW'[planeid])
         ax.set_ylabel('Pitch [mm]')
         ax.set_xlabel('Time [us]')
-        im = ax.pcolormesh(t/units.us, p/units.mm, c/units.picoampere,
+        im = ax.pcolormesh(t/units.us, p/units.mm, lg10(c/units.picoampere),
                                vmin=-vlim, vmax=vlim,
                                cmap='seismic')
         fig.colorbar(im, ax=[ax], shrink=0.9, pad=0.05)
 
-        for iwire in range(10):
-            ax.axhline(-iwire*3*units.mm, linewidth=1, color='black')
-            ax.axhline(+iwire*3*units.mm, linewidth=1, color='black')
-            ax.axhline(-(iwire+0.5)*3*units.mm, linewidth=1, color='gray',
-                       linestyle='dashed')
-            ax.axhline(+(iwire+0.5)*3*units.mm, linewidth=1, color='gray',
-                       linestyle='dashed')
+        # for iwire in range(10):
+        #     ax.axhline(-iwire*3*units.mm, linewidth=1, color='black')
+        #     ax.axhline(+iwire*3*units.mm, linewidth=1, color='black')
+        #     ax.axhline(-(iwire+0.5)*3*units.mm, linewidth=1, color='gray',
+        #                linestyle='dashed')
+        #     ax.axhline(+(iwire+0.5)*3*units.mm, linewidth=1, color='gray',
+        #                linestyle='dashed')
 
     if filename:
         print ("Saving to %s" % filename)
